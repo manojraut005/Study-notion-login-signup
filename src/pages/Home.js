31259d96cbb4dfce7ba49cbe99eb126518a4eef1
manojraut005 <102407303+manojraut005@users.noreply.github.com>
2023-12-03 17:18:56 +0530

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = ({isLoggedIn}) => {
+  return (
+    <div className='flex justify-center items-center text-white text-2xl md:text-3xl h-full'>
+      Login or Sign up to start...
+    </div>
+  )
+}
+
+export default Home
